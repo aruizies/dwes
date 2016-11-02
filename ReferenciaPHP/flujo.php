6dd6d6d46c8b1c74b99ec9815efde8f36a70950b
alumno <alumno@w7basica>
2016-11-02 09:28:56 +0100

    flujo

---
+++ ReferenciaPHP/flujo.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+<meta charset="utf-8">
+<title>Referencia PHP</title>
+</head>
+<body>
+<?php
+
+echo "<h1>Pruebas flujo</h1>";
+
+$num1 = 2;
+$bi = "true";
+switch($num1)
+{
+	case 1:
+	case 3: 
+	case 5:
+		echo "<h2>31 días</h2>";
+		break;
+	case 2:
+		if ($bi)
+		echo "<h2>28 días</h2>";
+		break;
+	case 4: 
+	case 6: 
+		echo "<h2>30 días</h2>";
+		break;
+	default:
+		echo "<h2>Error</h2>";
+
+}
+
+?>
+</body>
+</html>
