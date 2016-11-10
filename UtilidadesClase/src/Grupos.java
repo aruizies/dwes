25114cccbd08eae0d49798368d9fe409091273f8
aruiz <aruiz@ESCRITORIO>
2016-11-10 11:47:24 +0100

    grupos

--- UtilidadesClase/src/Grupos.java
+++ UtilidadesClase/src/Grupos.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Grupos {
 
@@ -10,20 +11,34 @@ public class Grupos {
 				"David", "Daniel Soriano", "América", "César", 
 				"Cristian", "Hugo",	"Daniel Vicho", "Sergio" };
 
-		ArrayList<String> alumnos = new ArrayList<String>();
+		int numMiembros = 3;
 
+		ArrayList<String> alumnos = new ArrayList<String>();
 		for (String nombre : nombres) {
 			alumnos.add(nombre);
 		}
-		
 		int numAlumnos = alumnos.size();
-		boolean impar = alumnos.size() % 2 == 0 ? false : true;
+		int sobrantes = numAlumnos % numMiembros;
+		System.out.println("Hay " + numAlumnos + " alumnos en clase y queremos grupos de "+numMiembros);
+		if (sobrantes > 0) System.out.println("Habrá " + sobrantes + " grupo/s con un@ alumn@ adicional.");
 
-		System.out.println("Hay " + numAlumnos + " alumnos en clase.");
+		Random generadorAleatorios = new Random(); 
 
-		for (int i = 1; i <= numAlumnos / 2; i++) {
+		for (int i = 1; i <= numAlumnos / numMiembros; i++) {
 			System.out.println("GRUPO " + i + ": ");
-
+			for (int j=0; j<numMiembros; j++) {
+				int aleatorio = generadorAleatorios.nextInt(alumnos.size());
+				System.out.println(" * " + alumnos.get(aleatorio));
+				alumnos.remove(aleatorio);
+				// Asignar alumn@ adicional si es necesario
+				if (sobrantes > 0)
+				{
+					aleatorio = generadorAleatorios.nextInt(alumnos.size());
+					System.out.println(" * " + alumnos.get(aleatorio));
+					alumnos.remove(aleatorio);
+					sobrantes --;
+				}
+			}
 		}
 	}
 
