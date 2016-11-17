56ea9d2deec90522bc44ffe22f32fb46bf1d0c42
aruiz <aruiz@ESCRITORIO>
2016-11-17 23:01:18 +0100

    h

--- Servlets/src/EjemploServletHttp.java
+++ Servlets/src/EjemploServletHttp.java
@@ -9,13 +9,13 @@ public class EjemploServletHttp extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 
-public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
-     res.setContentType("text/html");
-     PrintWriter out = res.getWriter();
-     out.println("<html><head><title>EjemploServletHttp</title></head>");
-     out.println("<body><h1>Mensaje desde el servlet HTTP de ejemplo</h1></body></html>");
-     out.close();
-  }
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		res.setContentType("text/html");
+		PrintWriter out = res.getWriter();
+		out.println("<html><head><title>EjemploServletHttp</title></head>");
+		out.println("<body><h1>Mensaje desde el servlet HTTP de ejemplo</h1></body></html>");
+		out.close();
+	}
 
 	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
 		doGet(req, res);
