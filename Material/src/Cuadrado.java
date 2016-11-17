d4b09ea04715cc86f74f1e30e7c96368c043e676
aruiz <aruiz@ESCRITORIO>
2016-11-17 21:16:02 +0100

    servlets

---
+++ Material/src/Cuadrado.java
@@ -0,0 +1,16 @@
+
+public class Cuadrado implements Figura {
+
+	private double lado;
+	
+	public Cuadrado(double lado) {
+		this.lado = lado;
+	}
+	public double calcularArea() {
+		return lado * lado;
+	}
+	public double calcularPerimetro() {
+		return 4*lado;
+	}
+}
+
---
+++ Material/src/Figura.java
@@ -0,0 +1,11 @@
+
+public interface Figura {
+
+	double PI = 3.1416; // por defecto "final" y "public"
+
+	double calcularArea(); // por defecto "public"
+	
+	double calcularPerimetro(); // por defecto "public"
+	
+}
+
